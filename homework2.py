abf4ec4d0f92c4faf9f3548eda0c5599f5a61a90
Tembulat100 <62437027+Tembulat100@users.noreply.github.com>
2020-09-17 18:52:33 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,7 +1,7 @@
 """Jack or King 8/52
 Jack and King 0/52
 Jack or 6 (шестёрка) or King 12/52
-Heart and Jack 0/52
-Heart and Jack 0/52
+Heart and Jack 1/52
+Heart and Jack 1/52
 Heart and (Jack or 6) 0/52
 heart and Jack and 6 0/52"""
