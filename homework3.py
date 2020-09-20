ee670327af3bcf0d1f961c461b5525a0995478ed
Tembulat100 <shogenov.tema@gmail.com>
2020-09-20 17:05:52 +0300

    Create homework3.py

---
+++ homework3.py
@@ -0,0 +1,20 @@
+""""
+P(Ace | Diamond) = 1/13
+P(Ace | King) = 1/4
+P(Ace | 6) = 1/4
+P(Ace | Ace) = 0/4
+P(6 | Spade) = 1/4
+P(Ace | Diamond) = 1/13
+
+Jack and Jack = 4/52
+Jack and King = 8/52
+no Jack and 7 = 4/52
+
+Jack, Jack, Jack = 4/52
+Jack, Jack, Jack, Jack, Jack = 0/52
+Jack, King, 6, 7 = 16/52
+2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace = 52/52
+Jack, no Jack, Jack, no Jack = 8/52
+"""
+
+
